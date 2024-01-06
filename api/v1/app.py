24fbc5849c7d38585920b080b208d7e4ca84689d
Soulsaw <ssawadogo292@gmail.com>
2024-01-06 23:24:10 +0000

    fix bug 1

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Doc for the module"""
-from flask import Flask, jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -18,7 +18,7 @@ def teardown(exce):
 @app.errorhandler(404)
 def not_found(error):
     """ json 404 page """
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
