809d72be289a8e3aae3732461291c031852feb92
Soulsaw <ssawadogo292@gmail.com>
2024-01-07 12:14:10 +0000

    fix hash

--- models/user.py
+++ models/user.py
@@ -2,8 +2,9 @@
 """ holds class User"""
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, event
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -25,3 +26,11 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+
+def generate_md5_hash(mapper, connection, target):
+    """ implement the md5 hash"""
+    target.password = md5(target.password.encode()).hexdigest()
+""" Listen the event on the table """
+event.listen(User, 'before_insert', generate_md5_hash)
+event.listen(User, 'before_update', generate_md5_hash)
